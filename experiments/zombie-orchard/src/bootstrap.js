5615f7e8b77fc4c4d419a8e8dc5cfc7473585b28
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-11 22:58:06 -0400

    Wire Zombie Orchard HUD into loop

--- experiments/zombie-orchard/src/bootstrap.js
+++ experiments/zombie-orchard/src/bootstrap.js
@@ -1,8 +1,9 @@
 import { createCanvasView } from "./canvas-view.js";
+import { createHud } from "./hud.js";
 import { createInputController } from "./input.js";
 import { createZombieOrchardSession } from "./session.js";
 import { startLoop } from "./runtime-loop.js";
 const game = createZombieOrchardSession();
 const input = createInputController(window);
 const view = createCanvasView(document.querySelector("#orchard-canvas"));
-startLoop(game, input, view, () => {});
+startLoop(game, input, view, createHud());
