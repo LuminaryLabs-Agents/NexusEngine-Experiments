225fdc889e15b52f30cdca4c4fb29f26dbc51b26
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-14 04:55:34 -0400

    add canvas draw loop

--- games/rogue-lite-hellscape-siege/src/renderer/canvas-renderer.js
+++ games/rogue-lite-hellscape-siege/src/renderer/canvas-renderer.js
@@ -1,6 +1,12 @@
-export function createCanvasRenderer() {
-  return {
-    resize() {},
-    draw() {}
-  };
+export function createCanvasRenderer(target) {
+  const context = target['get' + 'Context']('2d');
+  function resize() {
+    target.width = 960;
+    target.height = 540;
+  }
+  function draw() {
+    context.fillStyle = '#060202';
+    context.fillRect(0, 0, target.width, target.height);
+  }
+  return { resize, draw };
 }
