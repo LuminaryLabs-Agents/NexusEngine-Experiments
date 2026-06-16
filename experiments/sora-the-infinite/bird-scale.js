3cf9a614c38b6375989d036699fd09531bee964c
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-16 19:17:37 -0400

    Add Sora meter-based bird scale convention

---
+++ experiments/sora-the-infinite/bird-scale.js
@@ -0,0 +1,15 @@
+export const SORA_BIRD_SCALE = Object.freeze({
+  unit: "meter",
+  bodyLengthM: 0.5,
+  wingspanM: 1.3,
+  bodyRadiusM: 0.07,
+  headRadiusM: 0.08,
+  shoulderWidthM: 0.16,
+  innerWingLengthM: 0.34,
+  outerWingLengthM: 0.24,
+  tipLengthM: 0.08,
+  wingThicknessM: 0.012,
+  visualScale: 1
+});
+
+export default SORA_BIRD_SCALE;
