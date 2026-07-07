33b8307c993d19885449d4f5e221b1b2cab9416a
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-07 02:50:49 -0400

    Retire unused Cozy Island module

--- experiments/cozy-island/cozy-island-rail-loader.js
+++ experiments/cozy-island/cozy-island-rail-loader.js
@@ -1,10 +1,1 @@
-if (!Object.prototype.hasOwnProperty.call(Object.prototype, "rotation")) {
-  Object.defineProperty(Object.prototype, "rotation", {
-    configurable: true,
-    enumerable: false,
-    writable: true,
-    value: { x: 0, y: 0, z: 0 }
-  });
-}
-
-await import("./cozy-island-rail.js?v=scroll-rail-3");
+await import("./cozy-island-rail.js?v=bezier-player-rail-1");
