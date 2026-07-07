425c6b15390e2f66042310c9830a58debd68f498
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-07 02:27:47 -0400

    Remove unsafe Object prototype patch

--- experiments/cozy-island/cozy-island-rail-loader.js
+++ experiments/cozy-island/cozy-island-rail-loader.js
@@ -1,11 +1,1 @@
-if (!Object.prototype.hasOwnProperty.call(Object.prototype, "rotation")) {
-  Object.defineProperty(Object.prototype, "rotation", {
-    configurable: true,
-    enumerable: false,
-    get() {
-      return { x: 0, y: 0, z: 0 };
-    }
-  });
-}
-
-await import("./cozy-island-rail.js?v=scroll-rail-2");
+await import("./cozy-island-rail.js?v=scroll-rail-3");
