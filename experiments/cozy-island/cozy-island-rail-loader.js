09ea374e4caf2aad6feeb36c0e135b258d67ce81
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-07 02:28:15 -0400

    Use writable rotation fallback for rail boot

--- experiments/cozy-island/cozy-island-rail-loader.js
+++ experiments/cozy-island/cozy-island-rail-loader.js
@@ -1,1 +1,10 @@
+if (!Object.prototype.hasOwnProperty.call(Object.prototype, "rotation")) {
+  Object.defineProperty(Object.prototype, "rotation", {
+    configurable: true,
+    enumerable: false,
+    writable: true,
+    value: { x: 0, y: 0, z: 0 }
+  });
+}
+
 await import("./cozy-island-rail.js?v=scroll-rail-3");
